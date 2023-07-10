1b57b8c46e974c37eaa0c55c6047aa964b6ef23a
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-07-10 12:42:18 -0700

    added search bar structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 function Header() {
     return(
         <>
             <h1>Jammming:</h1>
             <h2>A Spotify Playlist Builder</h2>
+            <SearchBar />
         </>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchBar() {
+    const [userInput, setUserInput] = useState('');
 
+    function handleChange(e){
+        setUserInput(e.target.value)
+    }
+
+    return(
+        <>
+            <form>
+                <input
+                    onChange={handleChange}
+                    value={userInput}
+                    type="text"
+                    id="search"
+                />
+            </form>
+        </>
+        
+    );
 }
 
 export default SearchBar;
