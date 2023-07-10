8dd87761cd75fabd3b443a4423bb46ac0fd2eaa5
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-07-10 12:35:31 -0700

    created files and structure for components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return(
+        <>
+            <h1>Jammming:</h1>
+            <h2>A Spotify Playlist Builder</h2>
+        </>
+    )
+}
+
+export default Header;
